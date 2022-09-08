475481ba7a6aab1d5fa7dc1577220977141eb4a3
Ryan Housand <rhousand@gmail.com>
2022-09-07 20:47:06 -0400

    Added tmux, git, vim, zsh, oh-my-zsh

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -5,7 +5,53 @@
   # paths it should manage.
   home.username = "rhousand";
   home.homeDirectory = "/home/rhousand";
-
+  home.packages = with pkgs; [
+    tmux
+  ];
+  programs = {
+    vim = {
+      enable = true;
+      plugins = with pkgs.vimPlugins; [ 
+        vim-airline 
+        vim-nix
+      ];
+      settings = { ignorecase = true; };
+      extraConfig = ''
+        set mouse=a
+        colorscheme desert
+      '';
+    };
+    zsh = {
+      enable = true;
+      autocd = true;
+      enableAutosuggestions = true;
+      oh-my-zsh = {
+        enable = true;
+        theme = "robbyrussell";
+        extraConfig = ''
+          SHOW_AWS_PROMPT=true
+        '';
+        plugins = [
+          "git"
+          "sudo"
+          "docker"
+          "terraform"
+          "systemadmin"
+          "vi-mode"
+          "aws"
+        ];
+      };
+    };
+    git = {
+      enable = true;
+      userName = "Ryan Housand";
+      userEmail = "rhousand@gmail.com";
+      aliases = {
+        st = "status";
+        cm = "commit -m";
+      };
+    };
+  };
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
